af53ab3226a087cb6d5b5d10c300226274729053
anveshperumalla <anvesh.perumalla@gmail.com>
2022-01-23 13:50:57 -0500

    Anvesh wrote a paragraph.

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
 <h1> Welcome to GCFCoders! </h1>
+
+<p>GCFcoders is a technology outreach branch of Grace Christian Fellowship. GCFCoders is established to gather / teach /learn new technological skills for the advancement of God's kingdom.</p>
