4fc959075b6095779b87d2af42e6a51062505d8c
root <root@LAPTOP-FLL1G8MV.localdomain>
2022-01-23 13:50:57 -0500

    Added a couple things?

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <h1> Welcome to GCFCoders! </h1>
+
+<h2> Jesse's Section: </h2>
+<p> This is Jesse's very lame contribution to test how Git works in a collaborative environment. </p>
+
+<h2> What Jesse Thinks GCFCoders Is All About: </h2>
+<p> For me personally, it's all about learning computer stuff that I have no idea how to do and struggling the whole time, but everyone is very nice and helpful anyways so I get to have fun. </p>
